995b7ef26d736402a7d66aedd6b8474f2e8bd213
Unknown <jeenmathew26@gmail.com>
2018-11-22 20:46:50 +0530

    Variable: Getting Data From User

---
+++ Pandora's Box/Variable_GettingDataFromUser.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 20:43:28 2018
+
+@author: jmat
+"""
+
+greeting=input("Who woke me up from sleep ? State your name ")
+print("Oh Hello there ",greeting)
