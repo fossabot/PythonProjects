5a8c5e54301f4dca760b4fcb11a18fc39ef59da3
Unknown <jeenmathew26@gmail.com>
2018-11-22 20:52:12 +0530

    Learning Variables Datatypes

---
+++ Pandora's Box/Variable_Types.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 20:47:39 2018
+
+@author: jmat
+"""
+
+print("2 is a ",type(2))
+print("2.4 is a ",type(2.4))
+print("w is a ",type('w'))
+
+print("2 in double quotes is a ",type("2"))
