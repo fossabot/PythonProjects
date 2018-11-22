caeb58815c66aaa4e6bb3f1fe7a9909917b2375f
Unknown <jeenmathew26@gmail.com>
2018-11-22 20:54:16 +0530

    Fifty Years Age Program

---
+++ Pandora's Box/FiftyYearsAge.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 22 20:52:23 2018
+
+@author: jmat
+"""
+
+age=input("Enter your age: ")
+#new_age=age+50
+new_age=int(age)+50
+print("So in 50 years you will be ",new_age," years old")
