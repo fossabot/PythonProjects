d5a50516206a1b28e40a898de24301f694d01a34
Unknown <jeenmathew26@gmail.com>
2018-11-23 03:03:09 +0530

    Learning about Strings

---
+++ Pandora's Box/Strings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 23 02:56:57 2018
+
+@author: jmat
+"""
+
+c="Here"
+print(c)
+
+print ( c.replace("e","i") )
+
+print (c.replace("e","i",1))
+
+print(c.upper())
+
+##To know all the different functions on string: dir("")
+## To get info about any string function: help ("".replace)
