578b2f2ad1e0d5423277e83bb81995544e2d755a
Unknown <jeenmathew26@gmail.com>
2018-11-23 08:53:18 +0530

    Learning about Tuples

--- Pandora's Box/Tuples.py
+++ Pandora's Box/Tuples.py
@@ -5,3 +5,14 @@ Created on Fri Nov 23 08:47:37 2018
 @author: jmat
 """
 
+t=("Eugene Belford",3.14,"Spectre")
+print("The tuple is ",t)
+
+#The Indexing and spltting methods will also work here
+print("The first element is " ,t[0] )
+print("The last element is ", t[-1] )
+
+#Tuples are immutable
+#That is you cannot add/remove items from tuples
+#Tuples has less functions than lists since Tuples are immutable
+
