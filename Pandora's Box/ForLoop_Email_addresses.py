4d4ec567d720763db79fa0307641466a1868cc20
JMAT <jeenmathew26@gmail.com>
2018-11-24 08:53:32 +0530

    Learning For loops with email addresses example

---
+++ Pandora's Box/ForLoop_Email_addresses.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 08:47:42 2018
+
+@author: jmat
+"""
+
+email=["me@gmail.com","we@yahoo.com","they@yandex.com"]
+for i in email:
+    if "gmail" in i:
+        print("Here's that gmail addresses you asked for",i)
