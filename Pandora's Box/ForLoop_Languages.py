464072716776cca5447e908f65edf800157f1be8
JMAT <jeenmathew26@gmail.com>
2018-11-24 10:25:29 +0530

    Learning about For loops

---
+++ Pandora's Box/ForLoop_Languages.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 08:54:33 2018
+
+@author: jmat
+"""
+
+lang=["English","Hindi","Malayalam","French"]
+print("The languages that I know ")
+for i in lang:
+    print (i)
---
+++ Pandora's Box/ForLoop_good_marks.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 10:20:12 2018
+
+@author: jmat
+"""
+
+marks=[40,20,15,45,50]
+print ("Okay. I have the marks you scored in last sem. You can tell the following marks to your parents")
+for i in marks:
+    if i >35:
+        print(i)
+    
