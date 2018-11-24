7d8d9ae1fa68ee8304575eae874241d2ca231cfb
JMAT <jeenmathew26@gmail.com>
2018-11-24 10:40:43 +0530

    Implementing a simple password login with while loop

---
+++ Pandora's Box/WhileLoop_PasswordLogin.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 24 10:39:01 2018
+
+@author: jmat
+"""
+
+pas=""
+
+while pas != "python123":
+    pas=input("Enter the password: ")
+    if pas != "python123":
+        print (" Sorry ! Try again  " )
+    else:
+        print("Welcome Master ! Jarvis is at your service ")
+
+
+
