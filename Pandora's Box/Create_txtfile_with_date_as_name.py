eec421e03d55629bbb9c5470e94cef0ff83f1632
JMAT <jeenmathew26@gmail.com>
2018-11-25 19:18:13 +0530

    Create Logswith time as name

--- Pandora's Box/Create_txtfile_with_date_as_name.py
+++ Pandora's Box/Create_txtfile_with_date_as_name.py
@@ -6,8 +6,15 @@ Created on Sun Nov 25 19:05:00 2018
 """
 import datetime
 
-filename=datetime.datetime.now()
+now=datetime.datetime.now()
 
+def format_time_for_name(filename):
+    correct_format=now.strftime("%Y-%m-%d-%H-%M-%S-%f.txt")
+    return (correct_format)
 def create_file():
-    with open(filename,"w") as file:
+    name=format_time_for_name(now)
+    with open(name,"w") as file:
         file.write("")
+
+#format_time_for_name()
+create_file()
