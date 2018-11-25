cc492e76b68bde36e65523f2b8f2b1f428f36d22
JMAT <jeenmathew26@gmail.com>
2018-11-25 14:18:36 +0530

    Optimised old code

--- Pandora's Box/FileWriting_withLoops.py
+++ Pandora's Box/FileWriting_withLoops.py
@@ -9,10 +9,6 @@ Created on Sun Nov 25 12:01:47 2018
 file=open("writing_example_withLoops.txt","w")
 i=1
 while i <=20:
-    #line_content="Line"+str(i)
-    if i ==1:
-        file.write("Line "+str(i))
-    else:
-        file.write("\nLine "+str(i))
+    file.write("Line "+str(i)+"\n")
     i=i+1
 file.close()
