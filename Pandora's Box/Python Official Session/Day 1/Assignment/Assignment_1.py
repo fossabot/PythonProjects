c999a16cc2fd8ba3eb6915a282e4f3835b5cddbe
JMAT <jeenmathew26@gmail.com>
2018-11-27 18:42:57 +0530

    Python Training
    Organised the files

---
+++ Pandora's Box/Python Official Session/Day 1/Assignment/Assignment_1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 27 18:38:51 2018
+
+@author: jmat
+"""
+
+"""
+Q.Write a Python program to calculate the length of a string
+"""
+
+string=input("Please enter a string")
+print("The length of the string ", string," is ", len(string))
+
