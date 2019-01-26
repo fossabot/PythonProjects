104d8fc387109102924bc7d21534d6db3a462ba6
Unknown <jeenmathew26@gmail.com>
2019-01-26 06:54:05 +0530

    Creating About Page Html file

--- Building Website with Flask/Returning HTML Templates.py
+++ Building Website with Flask/Returning HTML Templates.py
@@ -16,7 +16,7 @@ def home():
 
 @app.route('/about/')
 def about():
-    return "Simple Website using Python and Flask!"
+    return render_template("about.html")
 
 if __name__=="__main__":
     app.run(debug=True)
