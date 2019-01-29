60108b3119fe8766eec987d53d0795591910c611
Unknown <jeenmathew26@gmail.com>
2019-01-29 21:49:38 +0530

    Viewing Data from Sqlite Database

--- Databases/Connecting and Inserting Data to SQlite via Python.py
+++ Databases/Connecting and Inserting Data to SQlite via Python.py
@@ -7,8 +7,27 @@ Created on Tue Jan 29 21:30:34 2019
 
 import sqlite3
 
-conn=sqlite3.connect("lite.db")
-cur=conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT , quantity INTEGER,  price REAL )")
-conn.commit()
-conn.close()
+def create_table():
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT , quantity INTEGER,  price REAL )")
+    cur.execute("INSERT INTO store VALUES ( 'Wine Glass',8,10.5)  ")
+    conn.commit()
+    conn.close()
+
+def insert(item,quantity,price):
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?,?,?)", (item,quantity,price))
+    conn.commit()
+    conn.close()
+    
+def view():
+    conn=sqlite3.connect("lite.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows=cur.fetchall()
+    cur.close()
+    return rows
+
+print(view())
