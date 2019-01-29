dc72ef63cd0c34790c29c00cabc62b9849f42919
Unknown <jeenmathew26@gmail.com>
2019-01-29 09:14:14 +0530

    Adding Buttons,Text to GUI

--- GUI with tkinter/Tkinter_101.py
+++ GUI with tkinter/Tkinter_101.py
@@ -9,4 +9,18 @@ from tkinter import *
 
 window=Tk()
 
+b1=Button(window,text="Subscribe to PewdiePie")
+
+#Either use pack or grid function to add the button to main window
+#b1.pack()
+b1.grid(row=0,column=0)
+#b1.grid(row=0,column=0,rowspan=2)
+
+
+e1=Entry(window)
+e1.grid(row=0, column=1)
+
+#t1=Text(window)
+t1=Text(window,height=2,width=20)
+t1.grid(row=0,column=2)
 window.mainloop()
