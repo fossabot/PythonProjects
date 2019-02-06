2006e9d3771dee32e6e3a2f3c685c12882791efd
JMAT <jeenmathew26@gmail.com>
2019-02-06 19:41:55 +0530

    Video Capture

--- Image and Video Processing with OpenCV/Face Detection/FaceDetection.py
+++ Image and Video Processing with OpenCV/Face Detection/FaceDetection.py
@@ -15,9 +15,15 @@ gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 
 faces=face_cascade.detectMultiScale(gray_img, scaleFactor=1.05,minNeighbors=5)
-print(faces)
-print(type(faces))
+#print(faces)
+#print(type(faces))
 
-cv2.imshow("Image",gray_img)
+
+for x,y,w,h in faces:
+    img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+
+resize_image=cv2.resize(  img,( int(img.shape[1]/3), int(img.shape[0]/3)  )  )
+
+cv2.imshow("Image",resize_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
